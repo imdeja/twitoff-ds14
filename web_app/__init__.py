d6f3de4b202d575363edfefd054ca6fded921d3c
imdeja <jakedennis877@gmail.com>
2020-05-18 17:18:32 -0700

    1

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -3,18 +3,24 @@ from flask import Flask
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
+from web_app.routes.tweet_routes import tweet_routes
 
-DATABASE_URI = "sqlite:///C:\\Users\\Voltaire22\\Desktop\\Voltaire22\\twitoff-ds14\\web_app_99.db"
-
+DATABASE_URI = "sqlite:///C:\\Users\\Jake\\twitoff-ds14\\web_app_99.db"
+SECRET_KEY = "super secret"
 def create_app():
     app = Flask(__name__)
 
+    app.config["SECRET_KEY"] = SECRET_KEY # required for flash messaging
+
+    # configure the database:
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # suppress warning messages
     db.init_app(app)
     migrate.init_app(app, db)
 
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
+    app.register_blueprint(tweet_routes)
     return app
 
 if __name__ == "__main__":
--- web_app/models.py
+++ web_app/models.py
@@ -12,20 +12,14 @@ class Book(db.Model):
     author_id = db.Column(db.String(128))
 
 class User(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True)
-    screen_name = db.Column(db.String(128), nullable=False)
-    name = db.Column(db.String)
-    location = db.Column(db.String)
-    followers_count = db.Column(db.Integer)
-    #latest_tweet_id = db.Column(db.BigInteger)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
 
 class Tweet(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True)
-    user_id = db.Column(db.BigInteger, db.ForeignKey("user.id"))
-    full_text = db.Column(db.String(500))
-    embedding = db.Column(db.PickleType)
-
-    user = db.relationship("User", backref=db.backref("tweets", lazy=True))
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Unicode(280), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship("User", backref=db.backref('tweets', lazy=True))
 
 def parse_records(database_records):
     """
